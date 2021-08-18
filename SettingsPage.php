76a1f6f3c5ed98a403bab6eea60f34071e348b87
Ben Turner <ben@polylab.co>
2021-08-18 17:44:58 +0930

    Remove deprecated function and adjust error handling

--- SettingsPage.php
+++ SettingsPage.php
@@ -4,11 +4,6 @@ use PixelKey\Algolia\Commands\RunIndexers;
 
 add_action( 'admin_menu', function() {
     add_options_page( 'Algolia Indexing', 'Algolia Indexing', 'manage_options', 'algolia-indexing', function() {
-        function getIndexerDisplayName($indexerName) {
-            $indexerParts = explode('\\', $indexerName);
-            return array_pop($indexerParts);
-        }
-
         echo "
             <h3>Algolia Indexing Control</h3>
             <div style='padding: 20px; background:#FFF; border: 1px solid #AAA; border-radius: 3px;'>
@@ -42,14 +37,14 @@ add_action( 'admin_menu', function() {
                 if(!in_array($indexerName, $indexerClasses)) {
                     throw new \Exception('Class does not exist as Indexer.');
                 }
-
                 $indexer = new $indexerName();
                 $indexer::index();
 
                 echo '<div style="padding:10px; background: #9ece7f; border: 1px solid #888; margin-bottom: 20px;">Running the <b>' . $indexer::DISPLAY_NAME . '</b> indexer... ✓</div>';
 
             } catch(\Exception $exception) {
-                echo $exception->getMessage();
+                echo '<div style="padding:10px; background: #ce7f7f; border: 1px solid #888; margin-bottom: 20px;">There has been a problem running the <b>' . $indexer::DISPLAY_NAME . '</b> indexer ❌</div>';
+                throw $exception; //Rethrow so it can be displayed or logged at the environments discretion
             }
         }
 
