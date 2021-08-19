2a012a8d7375eff11286332bd013967a75e04816
ehnto <49606979+ehnto@users.noreply.github.com>
2021-08-19 17:11:45 +0930

    Update PixelKeyAutoloader.php
    Make sure autoloading uses correct directory separators

--- PixelKeyAutoloader.php
+++ PixelKeyAutoloader.php
@@ -5,7 +5,7 @@ spl_autoload_register('pixelkey_autoloader');
 function pixelkey_autoloader($classname) {
     $dir = dirname(__FILE__);
     $parts = explode('\\', $classname);
-    $location = $dir . '/' . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
+    $location = $dir . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
 
     //If it's not in our namespace, other autoloaders should handle them
     if(!in_array('PixelKey', $parts)){
